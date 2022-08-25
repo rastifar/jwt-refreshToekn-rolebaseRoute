883e370647fcb94e432f0011323cd10478e11635
haniey <h.rastifar@gmail.com>
2022-08-25 09:29:11 -0700

    'first'

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Routing from "./Routing";
+
+function App() {
+  return (
+    <main className="App">
+      <Routing/>
+      {/* <Login /> */}
+      {/* <Register/> */}
+    </main>
+  );
+}
+
+export default App;
