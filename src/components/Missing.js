5502a62518d09b2b2b41c3b78dfddb9e6489933a
rastifar <rastifar@yahoo.com>
2024-10-10 05:53:38 -0700

    .

--- src/components/Missing.js
+++ src/components/Missing.js
@@ -1,18 +1,15 @@
-import { Link } from "react-router-dom";
-import { useRouteError } from "react-router-dom";
-const Missing = () => {
-  // return (
-  //     <article style={{ padding: "100px" }}>
-  //         <h1>Oops!</h1>
-  //         <p>Page Not Found</p>
-  //         <div className="flexGrow">
-  //             <Link to="/">Visit Our Homepage</Link>
-  //         </div>
-  //     </article>
-  // )
-  const error = useRouteError();
-  console.error(error);
-  return <div>Something went wrong!</div>;
-};
+import { Link } from "react-router-dom"
 
-export default Missing;
+const Missing = () => {
+    return (
+        <article style={{ padding: "100px" }}>
+            <h1>Oops!</h1>
+            <p>Page Not Found</p>
+            <div className="flexGrow">
+                <Link to="/">Visit Our Homepage</Link>
+            </div>
+        </article>
+    )
+}
+
+export default Missing
